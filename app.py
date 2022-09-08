84ddfe4b719f02310dac403aabb01eeed878eef0
JVJayarah3 <113084404+JVJayarah3@users.noreply.github.com>
2022-09-08 15:26:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,30 @@ def main():
         df.to_excel("https://github.com/JVJayarah3/Automated-Offset-well-analysis/main/test.xlsx")
        
 main()
-       
+
+import yagmail 
+
+send_ = input("READY TO SEND - (Y/N) - ")
+send_ = send_.upper()
+if send_ == "Y":
+    content = "Dear Sir, \n\t\t\t Kindly find the attached document. \n\n\n\n Thanks & Regards \n Jayaraj J V"
+    subject = "One Report - Jindal Star-"
+    subject = subject + str(final_date)
+    attachment = savepath+str(final_date)+'_JS.xlsx'
+    user = 'jvjayaraj7@gmail.com'
+    app_password = 'ceyo luws wsef fagr' # a token for gmail
+    to = star_dm
+    subject = subject
+    content = [content,attachment]
+    
+    print("SUBJECT : ",subject)
+    print("CONTENT : ",content)
+    send_ = input("READY TO SEND - (Y/N) - ")
+    send_ = send_.upper()
+    if send_ == "Y":
+        with yagmail.SMTP(user, app_password) as yag:
+            yag.send(to, subject, content)
+            print('Sent email successfully')
+    else:
+        print("E-mail not send")     
        
