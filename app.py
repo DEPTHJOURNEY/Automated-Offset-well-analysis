a826201dc2ef0de4724dbcc48e49c2189ed51075
JVJayarah3 <113084404+JVJayarah3@users.noreply.github.com>
2022-09-08 17:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,7 @@ def main():
         st.text("It may take couple of minutes")
         df = pd.DataFrame()
         df['Name'] = ['Abel','Bake','cook']
-        content = "Dear Sir, \n\t\t\t Kindly find the attached document. \n\n\n\n Thanks & Regards \n Jayaraj J V"
-        subject = "test-onlibe cloud"
-        subject = subject 
-        user = 'jvjayaraj7@gmail.com'
-        app_password = 'ceyo luws wsef fagr' # a token for gmail
-        to = "jvjayaraj4@gmail.com"
-        with yagmail.SMTP(user, app_password) as yag:
-            yag.send(to, subject, content)
-        
+        df.to_excel(test.xlsx)
        
 main()
 
