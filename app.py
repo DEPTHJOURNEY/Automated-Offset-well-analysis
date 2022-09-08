a46430202b81071820c5f8c813662d9e36e60bc7
JVJayarah3 <113084404+JVJayarah3@users.noreply.github.com>
2022-09-08 17:43:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def main():
         st.text("It may take couple of minutes")
         df = pd.DataFrame()
         df['Name'] = ['Abel','Bake','cook']
-        df.to_excel('jvjayarah3/automated-offset-well-analysis/main/test.xlsx')
+        df.to_excel('https://github.com/jvjayarah3/automated-offset-well-analysis/main/test.xlsx')
         st.text("done")
 main()
 
