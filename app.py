62c0509b399da8771d2e06b26f9d5bd1562758ec
JVJayarah3 <113084404+JVJayarah3@users.noreply.github.com>
2022-09-08 15:02:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title("AUTOMATED OFFSET WELL ANALYSIS")
+
+def main():
+    result1 = st.button("ANALYSE")
+    if result1:
+        st.header("PROCESSING")
+        st.text("It may take couple of minutes")
+        df = pd.DataFrame()
+        df['Name'] = ['Abel','Bake','cook']
+        df.to_excel("https://github.com/JVJayarah3/Automated-Offset-well-analysis/main/test.xlsx")
+       
+main()
+       
+       
