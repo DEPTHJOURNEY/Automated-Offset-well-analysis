5d42bfdfdb84dde8382d4222fffe94327e1bb51a
JVJayarah3 <113084404+JVJayarah3@users.noreply.github.com>
2022-09-09 11:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,25 @@ def main():
         st.text("It may take couple of minutes")
         for r in result:
             st.text(str(r))
+            if r.endswith('.pdf'):
+                onlypdfs.append(r)
+        
+        for x in onlypdfs:
+            pdf_file_obj =  x
+            #print(x)
+            #print(pdf_file_obj)
+            pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+            text = ""
+            for pagenum in range(pdf_reader.numPages):
+                pageobj = pdf_reader.getPage(pagenum)
+                text += pageobj.extractText()
+                #print(text)
+                #text= text.replace("\n"," ")
+                #print(text)
+                text= text.replace("\n"," ")
+                text1 = str(text)
         st.text("done")
+        st.text(text1)
 main()
 
    
