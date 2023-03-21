ddd00a299a3139195486d9b422c8d223fcb51292
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-03-21 22:34:18 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,8 +1,21 @@
-
-
+import pandas as pd
 import streamlit as st
-
 
 st.set_page_config(page_title='ONE-PLACE')
 st.header("TEST")
 
+
+
+
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.code} is :{row.IADC_DESC}:")
