9aacfc96c6509d95ae19808fbf47daf2b760b9e0
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-03-21 22:50:43 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,17 +4,17 @@ import streamlit as st
 st.set_page_config(page_title='ONE-PLACE')
 st.header("TEST")
 
-
+st.write("df is taking")
 
 
 # Read in data from the Google Sheet.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
-st.write("df is taking")
+
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
-
+st.write("df is taking")
 df = load_data(st.secrets["public_gsheets_url"])
 
 # Print results.
