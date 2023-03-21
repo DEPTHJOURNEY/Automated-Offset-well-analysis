34529d25cfa6b4d9f0aa901ebcd504635b615206
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-03-21 23:28:24 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -13,7 +13,7 @@ st.write("df is taking")
 
 def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url,error_bad_lines=False)
+    return pd.read_csv(csv_url)
 st.write("df is taking")
 df = load_data(st.secrets["public_gsheets_url"])
 st.write("df is taken")
