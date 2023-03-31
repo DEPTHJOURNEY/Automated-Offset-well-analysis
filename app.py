e7a3d02296772d07ccc62407f490a2de4cd2235e
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-04-01 01:19:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,21 +5,7 @@ from PIL import Image
 st.set_page_config(layout='centered')
 img_path = "https://github.com/JVJayarah3/Automated-Offset-well-analysis/blob/main/oil%20rig1.jpg"
 from streamlit_extras.switch_page_button import switch_page
-import base64
-"""def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-    )"""
+
 
    st.markdown(
     """
