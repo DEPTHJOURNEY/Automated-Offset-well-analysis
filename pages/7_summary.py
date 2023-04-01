d2f72d58a989cdc7fba20eab87a5e03e93455e86
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-04-01 13:38:48 +0530

    Update 7_summary.py

--- pages/7_summary.py
+++ pages/7_summary.py
@@ -10,9 +10,19 @@ import time
 from PIL import Image
 import base64
 from streamlit_extras.switch_page_button import switch_page
-st.set_page_config(layout='wide')
 import pandas as pd
+st.set_page_config(layout='wide',initial_sidebar_state="collapsed")
 
+st.markdown(
+    """
+<style>
+    [data-testid="collapsedControl"] {
+        display: none
+    }
+</style>
+""",
+    unsafe_allow_html=True,
+)
 path_1 = "https://github.com/JVJayarah3/Automated-Offset-well-analysis/blob/main/IADC_WELL_RPT_test.xlsx?raw=true"
 path_2 = "https://github.com/JVJayarah3/Automated-Offset-well-analysis/blob/main/IADC_WELL_RPT_rig_color.xlsx?raw=true"
 path_3 = "https://github.com/JVJayarah3/Automated-Offset-well-analysis/blob/main/IADC_WELL_RPT_reg_color.xlsx?raw=true"
