39f0c17bb409f97c268bb9afb8ad359887cc59e0
jvjayaraj4 <113084404+JVJayarah3@users.noreply.github.com>
2023-04-06 23:00:30 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -17,7 +17,7 @@ def load_data(sheets_url):
 st.write("df is taking")
 df = load_data(st.secrets["public_gsheets_url"])
 st.write("df is taken")
-#st.write(df)
+st.dataframe(df)
 # Print results.
 
 
